b1ae82ff9c65f96b6ce9bad8f3c41da204f29970
Kirari04 <lev@saminskij.ch>
2023-02-26 00:22:06 +0100

    added refresh jwt token controller

---
+++ controllers/AuthRefreshController.go
@@ -0,0 +1,22 @@
+package controllers
+
+import (
+	"ch/kirari04/videocms/auth"
+	"strings"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func AuthRefresh(c *fiber.Ctx) error {
+	bearer := c.GetReqHeaders()["Authorization"]
+	if bearer == "" {
+		return c.SendStatus(fiber.StatusForbidden)
+	}
+	bearerHeader := strings.Split(bearer, " ")
+	tokenString := bearerHeader[len(bearerHeader)-1]
+	newTokenString, err := auth.RefreshJWT(tokenString)
+	if err != nil {
+		return c.Status(fiber.StatusForbidden).SendString(err.Error())
+	}
+	return c.SendString(newTokenString)
+}
