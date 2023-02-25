ababeecb899bf376b4275ec33638e970083596bb
Kirari04 <lev@saminskij.ch>
2023-02-26 00:22:21 +0100

    added localhost ip to whitelist

--- helpers/LimiterWhitelistIps.go
+++ helpers/LimiterWhitelistIps.go
@@ -3,7 +3,7 @@ package helpers
 import "github.com/gofiber/fiber/v2"
 
 var LimiterWhitelistIps = map[string]bool{
-	// "127.0.0.1": true,
+	"127.0.0.1": true,
 }
 
 func LimiterWhitelistNext(c *fiber.Ctx) bool {
