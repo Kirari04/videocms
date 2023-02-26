83842d42595bd4602512909b02424c9053600f95
Kirari04 <lev@saminskij.ch>
2023-02-26 10:01:07 +0100

    grouped api auth

--- routes/api.go
+++ routes/api.go
@@ -6,23 +6,21 @@ import (
 	"ch/kirari04/videocms/inits"
 	"time"
 
-	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/limiter"
 )
 
 func Api() {
-	inits.Api.Get("", func(c *fiber.Ctx) error {
-		return c.SendString("Salamn")
-	})
+	inits.Api.Use(limiter.New(*helpers.LimiterConfig(10, time.Second)))
 
-	inits.Api.
-		Use(limiter.New(*helpers.LimiterConfig(10, time.Hour))).
-		Post("/login", controllers.Login)
+	auth := inits.Api.Group("/auth")
+	auth.Use(limiter.New(*helpers.LimiterConfig(10, time.Hour))).
+		Post("/login", controllers.AuthLogin)
 
-	inits.Api.
-		Use(limiter.New(*helpers.LimiterConfig(1, time.Second*10))).
-		Get("/auth/check", controllers.AuthCheck)
-	inits.Api.
-		Use(limiter.New(*helpers.LimiterConfig(1, time.Minute))).
-		Get("/auth/refresh", controllers.AuthRefresh)
+	auth.Use(limiter.New(*helpers.LimiterConfig(1, time.Second*10))).
+		Get("/check", controllers.AuthCheck)
+	auth.Use(limiter.New(*helpers.LimiterConfig(1, time.Minute))).
+		Get("/refresh", controllers.AuthRefresh)
+
+	folders := inits.Api.Group("/folders")
+	folders.Get("/list", controllers.ListFolders)
 }
