5e1b9d9f1d06ee21ba4da9f7baad5238b141b89b
Kirari04 <lev@saminskij.ch>
2023-02-26 18:44:18 +0100

    list files / links controller

---
+++ controllers/ListFilesController.go
@@ -0,0 +1,61 @@
+package controllers
+
+import (
+	"ch/kirari04/videocms/helpers"
+	"ch/kirari04/videocms/inits"
+	"ch/kirari04/videocms/models"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func ListFiles(c *fiber.Ctx) error {
+	// parse & validate request
+	var fileValidation models.FileListValidation
+	if err := c.QueryParser(&fileValidation); err != nil {
+		return c.Status(400).JSON([]helpers.ValidationError{
+			{
+				FailedField: "none",
+				Tag:         "none",
+				Value:       "Invalid body request format",
+			},
+		})
+	}
+
+	if errors := helpers.ValidateStruct(fileValidation); len(errors) > 0 {
+		return c.Status(400).JSON(errors)
+	}
+
+	//check if requested folder exists
+	if fileValidation.ParentFolderID > 0 {
+		res := inits.DB.First(&models.Folder{}, fileValidation.ParentFolderID)
+		if res.Error != nil {
+			return c.Status(400).JSON([]helpers.ValidationError{
+				{
+					FailedField: "ParentFolderID",
+					Tag:         "exists",
+					Value:       "Parent folder doesn't exist",
+				},
+			})
+		}
+	}
+
+	// query all files
+	var links []models.Link
+	res := inits.DB.
+		Model(&models.Link{}).
+		Preload("User").
+		Preload("File").
+		Where(&models.Link{
+			ParentFolderID: fileValidation.ParentFolderID,
+			UserID:         c.Locals("UserID").(uint),
+		}, "ParentFolderID", "UserID").
+		Find(&links)
+	if res.Error != nil {
+		log.Fatalf("Failed to query file list: %v", res.Error)
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	// return value
+	return c.JSON(links)
+}
