2e1d1ca91b7a90cf2b41116bd8da0de5a66f1778
Kirari04 <lev@saminskij.ch>
2023-03-02 16:40:18 +0100

    added audio files controller

---
+++ controllers/GetAudioDataController.go
@@ -0,0 +1,61 @@
+package controllers
+
+import (
+	"ch/kirari04/videocms/helpers"
+	"ch/kirari04/videocms/inits"
+	"ch/kirari04/videocms/models"
+	"fmt"
+	"os"
+	"regexp"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func GetAudioData(c *fiber.Ctx) error {
+	type Request struct {
+		UUID    string `validate:"required,uuid_rfc4122"`
+		SUBUUID string `validate:"required,uuid_rfc4122"`
+		FILE    string `validate:"required"`
+	}
+	var requestValidation Request
+	if err := c.ParamsParser(&requestValidation); err != nil {
+		return c.Status(400).JSON([]helpers.ValidationError{
+			{
+				FailedField: "none",
+				Tag:         "none",
+				Value:       "Invalid body request format",
+			},
+		})
+	}
+
+	if errors := helpers.ValidateStruct(requestValidation); len(errors) > 0 {
+		return c.Status(400).JSON(errors)
+	}
+
+	reFILE := regexp.MustCompile(`^audio[0-9]{0,4}\.(m3u8|ts)$`)
+
+	if !reFILE.MatchString(requestValidation.FILE) {
+		return c.Status(400).SendString("Bad file format")
+	}
+
+	//translate link id to file id
+	var dbLink models.Link
+
+	if dbRes := inits.DB.
+		Model(&models.Link{}).
+		Preload("File").
+		Where(&models.Link{
+			UUID: requestValidation.UUID,
+		}).
+		First(&dbLink); dbRes.Error != nil {
+		return c.Status(fiber.StatusNotFound).SendString("Link doesn't exist")
+	}
+
+	filePath := fmt.Sprintf("./videos/qualitys/%s/%s/%s", dbLink.File.UUID, requestValidation.SUBUUID, requestValidation.FILE)
+
+	file, err := os.Open(filePath)
+	if err != nil {
+		return c.Status(fiber.StatusNotFound).SendString("File not found - lol")
+	}
+	return c.SendStream(file)
+}
