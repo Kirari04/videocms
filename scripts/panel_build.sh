1bb3d28ffe2efad12968d907a382b667be814168
Kirari04 <lev@saminskij.ch>
2023-03-05 14:10:55 +0100

    removed demo container

--- scripts/panel_build.sh
+++ scripts/panel_build.sh
@@ -1,4 +1,4 @@
 echo RUNNING DOCKER BUILD AMD64 PANEL
 docker build . --platform linux/amd64 -f Dockerfile.panel -t kirari04/videocms:panel --push
 echo RUNNING DOCKER BUILD ARM64 PANEL
-docker build . --platform linux/arm64 -f Dockerfile.arm64.panel -t kirari04/videocms:panel_arm64 --push
+docker build . --platform linux/arm64 -f Dockerfile.panel.arm64 -t kirari04/videocms:panel_arm64 --push
