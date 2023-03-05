8d20e3fc15ef81982d583777639898646b681b92
Kirari04 <lev@saminskij.ch>
2023-03-05 14:40:58 +0100

    added all demo builds

--- scripts/demo_build.sh
+++ scripts/demo_build.sh
@@ -3,6 +3,6 @@ docker build . --platform linux/amd64 -f Dockerfile.demo -t kirari04/videocms:de
 echo RUNNING DOCKER BUILD ARM64 DEMO
 docker build . --platform linux/arm64 -f Dockerfile.demo.arm64 -t kirari04/videocms:demo_arm64 --push
 echo RUNNING DOCKER BUILD AMD64 DEMO PANEL
-docker build . --platform linux/amd64 -f Dockerfile.panel.demo -t kirari04/videocms:demo_panel --push
+docker build . --platform linux/amd64 -f Dockerfile.demo.panel -t kirari04/videocms:demo_panel --push
 echo RUNNING DOCKER BUILD ARM64 DEMO PANEL
-docker build . --platform linux/arm64 -f Dockerfile.panel.demo.arm64 -t kirari04/videocms:demo_panel_arm64 --push
+docker build . --platform linux/arm64 -f Dockerfile.demo.panel.arm64 -t kirari04/videocms:demo_panel_arm64 --push
