6c7c69fee741cc2621c183d9f91a09806fa26be1
Kirari04 <lev@saminskij.ch>
2023-03-05 15:12:14 +0100

    split up demo and demo panel

--- scripts/demo_build.sh
+++ scripts/demo_build.sh
@@ -2,7 +2,3 @@ echo RUNNING DOCKER BUILD AMD64 DEMO
 docker build . --platform linux/amd64 -f Dockerfile.demo -t kirari04/videocms:demo --push
 echo RUNNING DOCKER BUILD ARM64 DEMO
 docker build . --platform linux/arm64 -f Dockerfile.demo.arm64 -t kirari04/videocms:demo_arm64 --push
-echo RUNNING DOCKER BUILD AMD64 DEMO PANEL
-docker build . --platform linux/amd64 -f Dockerfile.demo.panel -t kirari04/videocms:demo_panel --push
-echo RUNNING DOCKER BUILD ARM64 DEMO PANEL
-docker build . --platform linux/arm64 -f Dockerfile.demo.panel.arm64 -t kirari04/videocms:demo_panel_arm64 --push
---
+++ scripts/demo_panel_build.sh
@@ -0,0 +1,4 @@
+echo RUNNING DOCKER BUILD AMD64 DEMO PANEL
+docker build . --platform linux/amd64 -f Dockerfile.demo.panel -t kirari04/videocms:demo_panel --push
+echo RUNNING DOCKER BUILD ARM64 DEMO PANEL
+docker build . --platform linux/arm64 -f Dockerfile.demo.panel.arm64 -t kirari04/videocms:demo_panel_arm64 --push
