26522ae59cee6ef4bea631cd97394072e8e694e3
Kirari04 <lev@saminskij.ch>
2023-03-06 19:41:47 +0100

    run bash commands concurrent

--- scripts/all.sh
+++ scripts/all.sh
@@ -1,6 +1,9 @@
 export DOCKER_BUILDKIT=1
 echo RUNNING DOCKER BUILD
-bash ./scripts/build.sh
-bash ./scripts/panel_build.sh
-bash ./scripts/demo_build.sh
-bash ./scripts/demo_panel_build.sh
+bash ./scripts/build.sh &> ./scripts/log/build.log &
+sleep 1
+bash ./scripts/panel_build.sh &> ./scripts/log/panel_build.log &
+sleep 1
+bash ./scripts/demo_build.sh &> ./scripts/log/demo_build.log &
+sleep 1
+bash ./scripts/demo_panel_build.sh &> ./scripts/log/demo_panel_build.log &
