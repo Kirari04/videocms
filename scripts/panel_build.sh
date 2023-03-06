a07c08b818a2e1ca5f0cbdd39de3b4ef25ec8884
Kirari04 <lev@saminskij.ch>
2023-03-06 20:58:48 +0100

    added done echo

--- scripts/panel_build.sh
+++ scripts/panel_build.sh
@@ -2,3 +2,7 @@ echo RUNNING DOCKER BUILD AMD64 PANEL
 docker build . --platform linux/amd64 -f Dockerfile.panel -t kirari04/videocms:panel --push
 echo RUNNING DOCKER BUILD ARM64 PANEL
 docker build . --platform linux/arm64 -f Dockerfile.panel.arm64 -t kirari04/videocms:panel_arm64 --push
+
+echo "===================================="
+echo "================DONE================"
+echo "===================================="
