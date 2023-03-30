ea6cee60c0b8148382a9e64c95720616910a1ece
Kirari04 <lev@saminskij.ch>
2023-03-30 16:14:39 +0200

    refactor CreateFolder controller

--- controllers/CreateFolderController.go
+++ controllers/CreateFolderController.go
@@ -14,11 +14,11 @@ func CreateFolder(c *fiber.Ctx) error {
 
 	var folderValidation models.FolderCreateValidation
 	if err := c.BodyParser(&folderValidation); err != nil {
-		return c.Status(400).SendString("Invalid body request format")
+		return c.Status(fiber.StatusBadRequest).SendString("Invalid body request format")
 	}
 
 	if errors := helpers.ValidateStruct(folderValidation); len(errors) > 0 {
-		return c.Status(400).SendString(fmt.Sprintf("%s [%s] : %s", errors[0].FailedField, errors[0].Tag, errors[0].Value))
+		return c.Status(fiber.StatusBadRequest).SendString(fmt.Sprintf("%s [%s] : %s", errors[0].FailedField, errors[0].Tag, errors[0].Value))
 	}
 
 	status, dbFolder, err := logic.CreateFolder(folderValidation.Name, folderValidation.ParentFolderID, c.Locals("UserID").(uint))
