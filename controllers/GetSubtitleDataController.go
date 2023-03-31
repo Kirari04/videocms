31b5c9de4c6b6846dd92dc09da83d130946cb763
Kirari04 <lev@saminskij.ch>
2023-03-31 13:46:11 +0200

    refactored GetSubtitleData controller

--- controllers/GetSubtitleDataController.go
+++ controllers/GetSubtitleDataController.go
@@ -2,10 +2,8 @@ package controllers
 
 import (
 	"ch/kirari04/videocms/helpers"
-	"ch/kirari04/videocms/inits"
-	"ch/kirari04/videocms/models"
+	"ch/kirari04/videocms/logic"
 	"fmt"
-	"regexp"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -18,55 +16,20 @@ func GetSubtitleData(c *fiber.Ctx) error {
 	}
 	var requestValidation Request
 	if err := c.ParamsParser(&requestValidation); err != nil {
-		return c.Status(400).JSON([]helpers.ValidationError{
-			{
-				FailedField: "none",
-				Tag:         "none",
-				Value:       "Invalid body request format",
-			},
-		})
+		return c.Status(fiber.StatusBadRequest).SendString("Invalid body request format")
 	}
 
 	if errors := helpers.ValidateStruct(requestValidation); len(errors) > 0 {
-		return c.Status(400).JSON(errors)
+		return c.Status(fiber.StatusBadRequest).SendString(fmt.Sprintf("%s [%s] : %s", errors[0].FailedField, errors[0].Tag, errors[0].Value))
 	}
 
-	reFILE := regexp.MustCompile(`^out\.(ass)$`)
-
-	if !reFILE.MatchString(requestValidation.FILE) {
-		return c.Status(400).SendString("Bad file format")
+	status, filePath, err := logic.GetSubtitleData(requestValidation.FILE, requestValidation.UUID, requestValidation.SUBUUID)
+	if err != nil {
+		return c.Status(status).SendString(err.Error())
 	}
 
-	//translate link id to file id
-	var dbLink models.Link
-
-	if dbRes := inits.DB.
-		Model(&models.Link{}).
-		Preload("File").
-		Preload("File.Subtitles").
-		Where(&models.Link{
-			UUID: requestValidation.UUID,
-		}).
-		First(&dbLink); dbRes.Error != nil {
-		return c.Status(fiber.StatusNotFound).SendString("Subtitle doesn't exist")
-	}
-
-	//check if subtitle uuid exists
-	subExists := false
-	for _, sub := range dbLink.File.Subtitles {
-		if sub.Ready &&
-			sub.UUID == requestValidation.SUBUUID {
-			subExists = true
-		}
-	}
-	if !subExists {
-		return c.Status(fiber.StatusNotFound).SendString("Subtitle doesn't exist")
-	}
-
-	filePath := fmt.Sprintf("./videos/qualitys/%s/%s/%s", dbLink.File.UUID, requestValidation.SUBUUID, requestValidation.FILE)
-
-	if err := c.SendFile(filePath); err != nil {
-		return c.Status(fiber.StatusNotFound).SendString("Subtitle doesn't exist")
+	if err := c.SendFile(*filePath); err != nil {
+		return c.Status(fiber.StatusNotFound).SendString("Subtitle file not found")
 	}
 	return nil
 }
---
+++ logic/GetSubtitleData.go
@@ -0,0 +1,49 @@
+package logic
+
+import (
+	"ch/kirari04/videocms/inits"
+	"ch/kirari04/videocms/models"
+	"errors"
+	"fmt"
+	"regexp"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func GetSubtitleData(fileName string, UUID string, SUBUUID string) (status int, filePath *string, err error) {
+	reFILE := regexp.MustCompile(`^out\.(ass)$`)
+
+	if !reFILE.MatchString(fileName) {
+		return fiber.StatusBadRequest, nil, errors.New("bad file format")
+	}
+
+	//translate link id to file id
+	var dbLink models.Link
+
+	if dbRes := inits.DB.
+		Model(&models.Link{}).
+		Preload("File").
+		Preload("File.Subtitles").
+		Where(&models.Link{
+			UUID: UUID,
+		}).
+		First(&dbLink); dbRes.Error != nil {
+		return fiber.StatusNotFound, nil, errors.New("subtitle doesn't exist")
+	}
+
+	//check if subtitle uuid exists
+	subExists := false
+	for _, sub := range dbLink.File.Subtitles {
+		if sub.Ready &&
+			sub.UUID == SUBUUID {
+			subExists = true
+		}
+	}
+	if !subExists {
+		return fiber.StatusNotFound, nil, errors.New("subtitle doesn't exist")
+	}
+
+	fileRes := fmt.Sprintf("./videos/qualitys/%s/%s/%s", dbLink.File.UUID, SUBUUID, fileName)
+
+	return fiber.StatusOK, &fileRes, nil
+}
