98282ca0c16983a75fb8dca42b9efcd8ac7b2a23
Kirari04 <lev@saminskij.ch>
2023-03-31 13:26:16 +0200

    added better GetM3u8Data validation log

--- controllers/GetM3u8DataController.go
+++ controllers/GetM3u8DataController.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"ch/kirari04/videocms/helpers"
 	"ch/kirari04/videocms/logic"
+	"fmt"
 
 	"github.com/gofiber/fiber/v2"
 )
@@ -16,13 +17,13 @@ func GetM3u8Data(c *fiber.Ctx) error {
 	if requestValidation.AUDIOUUID != "" {
 		// validate audio stream
 		if errors := helpers.ValidateStruct(requestValidation); len(errors) > 0 {
-			return c.Status(400).JSON(errors)
+			return c.Status(fiber.StatusBadRequest).SendString(fmt.Sprintf("%s [%s] : %s", errors[0].FailedField, errors[0].Tag, errors[0].Value))
 		}
 	} else {
 		// validate muted stream
 		requestValidationMuted.UUID = requestValidation.UUID
 		if errors := helpers.ValidateStruct(requestValidationMuted); len(errors) > 0 {
-			return c.Status(400).JSON(errors)
+			return c.Status(fiber.StatusBadRequest).SendString(fmt.Sprintf("%s [%s] : %s", errors[0].FailedField, errors[0].Tag, errors[0].Value))
 		}
 	}
 
