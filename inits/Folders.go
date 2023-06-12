455c553093bf17fa00717c06d5d509df7254b7b3
Kirari04 <lev@saminskij.ch>
2023-06-12 13:21:12 +0200

    made the folders more secure

--- inits/Folders.go
+++ inits/Folders.go
@@ -10,7 +10,7 @@ func Folders() {
 	createFolders := []string{"./database", "./videos", "./videos/uploads", "./logs"}
 	for _, createFolder := range createFolders {
 		if fileInfo, err := os.Stat(createFolder); err != nil || !fileInfo.IsDir() {
-			if err := os.Mkdir(createFolder, 0777); err != nil {
+			if err := os.Mkdir(createFolder, 0766); err != nil {
 				log.Panic("Failed to generate essential folder")
 			}
 			log.Printf("Generated folder: %s\n", createFolder)
