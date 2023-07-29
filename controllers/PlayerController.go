6869afaa751dbb1652bc3e93a0b2a91747fe2e69
Kirari04 <lev@saminskij.ch>
2023-07-29 15:40:23 +0200

    added account webhook list to player controller

--- controllers/PlayerController.go
+++ controllers/PlayerController.go
@@ -8,6 +8,7 @@ import (
 	"encoding/base64"
 	"encoding/json"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
 
@@ -30,7 +31,7 @@ func PlayerController(c *fiber.Ctx) error {
 
 	//check if requested folder exists
 	var dbLink models.Link
-	res := inits.DB.
+	if res := inits.DB.
 		Preload("File").
 		Preload("File.Qualitys").
 		Preload("File.Subtitles").
@@ -38,12 +39,11 @@ func PlayerController(c *fiber.Ctx) error {
 		Where(&models.Link{
 			UUID: requestValidation.UUID,
 		}).
-		First(&dbLink)
-	if res.Error != nil {
+		First(&dbLink); res.Error != nil {
 		return c.Status(fiber.StatusNotFound).Render("404", fiber.Map{})
 	}
 
-	//check if has some file is ready
+	// List qualitys non hls & check if has some file is ready
 	var streamIsReady bool
 	var jsonQualitys []map[string]string
 	for _, qualiItem := range dbLink.File.Qualitys {
@@ -59,9 +59,9 @@ func PlayerController(c *fiber.Ctx) error {
 			}
 		}
 	}
-
 	rawQuality, _ := json.Marshal(jsonQualitys)
 
+	// List subtitles
 	var jsonSubtitles []map[string]string
 	for _, subItem := range dbLink.File.Subtitles {
 		if subItem.Ready {
@@ -78,6 +78,7 @@ func PlayerController(c *fiber.Ctx) error {
 	}
 	rawSubtitles, _ := json.Marshal(jsonSubtitles)
 
+	// List audios
 	var jsonAudios []map[string]string
 	for _, audioItem := range dbLink.File.Audios {
 		if audioItem.Ready {
@@ -92,6 +93,28 @@ func PlayerController(c *fiber.Ctx) error {
 	}
 	rawAudios, _ := json.Marshal(jsonAudios)
 
+	// List webhooks
+	var webhooks []models.Webhook
+	if res := inits.DB.
+		Where(&models.Webhook{
+			UserID: dbLink.UserID,
+		}).
+		Find(&webhooks); res.Error != nil {
+		log.Printf("Failed to query webhooks of file owner: %v", res.Error)
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+	var jsonWebhooks []map[string]any
+	for _, webhookItem := range webhooks {
+		jsonWebhooks = append(jsonWebhooks, map[string]any{
+			"name":     webhookItem.Name,
+			"url":      webhookItem.Url,
+			"rpm":      webhookItem.Rpm,
+			"reqQuery": webhookItem.ReqQuery,
+			"resField": webhookItem.ResField,
+		})
+	}
+	rawWebhooks, _ := json.Marshal(jsonWebhooks)
+
 	return c.Render("player", fiber.Map{
 		"Title":         fmt.Sprintf("%s - %s", config.ENV.AppName, dbLink.Name),
 		"Description":   fmt.Sprintf("Watch %s on %s", dbLink.Name, config.ENV.AppName),
@@ -101,6 +124,7 @@ func PlayerController(c *fiber.Ctx) error {
 		"Qualitys":      string(rawQuality),
 		"Subtitles":     string(rawSubtitles),
 		"Audios":        string(rawAudios),
+		"Webhooks":      string(rawWebhooks),
 		"StreamIsReady": streamIsReady,
 		"UUID":          requestValidation.UUID,
 		"PROJECTURL":    config.ENV.Project,
