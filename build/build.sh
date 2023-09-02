9fd5f32a747c3f7f7f41a8f0f1770b48951986ee
kirari04 <lev@saminskij.ch>
2023-09-02 14:33:00 +0200

    Alphine Support  - dropped support for windows  - added support for alphine docker images

--- build/build.sh
+++ build/build.sh
@@ -1,51 +1,61 @@
 #!/bin/bash
-rm -rf ./build/cmd
-rm -fr ./build/svelte
 bash scripts/\$default.sh
 
-# BINARIES
-mkdir -p ./build/cmd
-# MAIN
-echo RUNNING GO BUILD MAIN
-## amd64
-echo RUNNING GO BUILD windows amd64
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o build/cmd/main_windows_amd64.exe main.go
-sha256sum build/cmd/main_windows_amd64.exe > build/cmd/main_windows_amd64.exe.sha256sum
-gpg --detach-sig --armor build/cmd/main_windows_amd64.exe
-echo RUNNING GO BUILD linux amd64
-CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o build/cmd/main_linux_amd64.bin main.go
-sha256sum build/cmd/main_linux_amd64.bin > build/cmd/main_linux_amd64.bin.sha256sum
-gpg --detach-sig --armor build/cmd/main_linux_amd64.bin
-## arm64
-echo RUNNING GO BUILD linux arm64
-CGO_ENABLED=1 CC=aarch64-linux-gnu-gcc-11 CC_FOR_TARGET=gcc-11-aarch64-linux-gnu GOOS=linux GOARCH=arm64 go build -o build/cmd/main_linux_arm64.bin main.go
-sha256sum build/cmd/main_linux_arm64.bin > build/cmd/main_linux_arm64.bin.sha256sum
-gpg --detach-sig --armor build/cmd/main_linux_arm64.bin
+# Prompt the user for confirmation
+read -p "Do you want to run the Binary Build Script? (yes/no): " answerbin
+if [ "$answerbin" = "yes" ]; then
+    rm -rf ./build/cmd
+    # BINARIES
+    mkdir -p ./build/cmd
+    # MAIN
+    echo RUNNING GO BUILD MAIN
+    ## amd64
+    # echo RUNNING GO BUILD windows amd64
+    # CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-linkmode external -extldflags -static" -a -installsuffix cgo -o build/cmd/main_windows_amd64.exe main.go
+    # sha256sum build/cmd/main_windows_amd64.exe > build/cmd/main_windows_amd64.exe.sha256sum
+    # gpg --detach-sig --armor build/cmd/main_windows_amd64.exe
+    echo RUNNING GO BUILD linux amd64
+    CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -ldflags "-linkmode external -extldflags -static" -a -installsuffix cgo -o build/cmd/main_linux_amd64.bin main.go
+    sha256sum build/cmd/main_linux_amd64.bin > build/cmd/main_linux_amd64.bin.sha256sum
+    gpg --detach-sig --armor build/cmd/main_linux_amd64.bin
+    ## arm64
+    echo RUNNING GO BUILD linux arm64
+    CGO_ENABLED=1 CC=aarch64-linux-gnu-gcc-11 CC_FOR_TARGET=gcc-11-aarch64-linux-gnu GOOS=linux GOARCH=arm64 go build -ldflags "-linkmode external -extldflags -static" -a -installsuffix cgo -o build/cmd/main_linux_arm64.bin main.go
+    sha256sum build/cmd/main_linux_arm64.bin > build/cmd/main_linux_arm64.bin.sha256sum
+    gpg --detach-sig --armor build/cmd/main_linux_arm64.bin
 
-# Console
-echo RUNNING GO BUILD CONSOLE
-## amd64
-echo RUNNING GO BUILD windows amd64
-CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -o build/cmd/console_windows_amd64.exe console/console.go
-sha256sum build/cmd/console_windows_amd64.exe > build/cmd/console_windows_amd64.exe.sha256sum
-gpg --detach-sig --armor build/cmd/console_windows_amd64.exe
-echo RUNNING GO BUILD linux amd64
-CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -o build/cmd/console_linux_amd64.bin console/console.go
-sha256sum build/cmd/console_linux_amd64.bin > build/cmd/console_linux_amd64.bin.sha256sum
-gpg --detach-sig --armor build/cmd/console_linux_amd64.bin
-## arm64
-echo RUNNING GO BUILD linux arm64
-CGO_ENABLED=1 CC=aarch64-linux-gnu-gcc-11 CC_FOR_TARGET=gcc-11-aarch64-linux-gnu GOOS=linux GOARCH=arm64 go build -o build/cmd/console_linux_arm64.bin console/console.go
-sha256sum build/cmd/console_linux_arm64.bin > build/cmd/console_linux_arm64.bin.sha256sum
-gpg --detach-sig --armor build/cmd/console_linux_arm64.bin
+    # Console
+    echo RUNNING GO BUILD CONSOLE
+    ## amd64
+    # echo RUNNING GO BUILD windows amd64
+    # CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-linkmode external -extldflags -static" -a -installsuffix cgo -o build/cmd/console_windows_amd64.exe console/console.go
+    # sha256sum build/cmd/console_windows_amd64.exe > build/cmd/console_windows_amd64.exe.sha256sum
+    # gpg --detach-sig --armor build/cmd/console_windows_amd64.exe
+    echo RUNNING GO BUILD linux amd64
+    CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -ldflags "-linkmode external -extldflags -static" -a -installsuffix cgo -o build/cmd/console_linux_amd64.bin console/console.go
+    sha256sum build/cmd/console_linux_amd64.bin > build/cmd/console_linux_amd64.bin.sha256sum
+    gpg --detach-sig --armor build/cmd/console_linux_amd64.bin
+    ## arm64
+    echo RUNNING GO BUILD linux arm64
+    CGO_ENABLED=1 CC=aarch64-linux-gnu-gcc-11 CC_FOR_TARGET=gcc-11-aarch64-linux-gnu GOOS=linux GOARCH=arm64 go build -ldflags "-linkmode external -extldflags -static" -a -installsuffix cgo -o build/cmd/console_linux_arm64.bin console/console.go
+    sha256sum build/cmd/console_linux_arm64.bin > build/cmd/console_linux_arm64.bin.sha256sum
+    gpg --detach-sig --armor build/cmd/console_linux_arm64.bin
+fi
 
-# PANEL
-git clone https://github.com/02Gqbriel/videocms-svelte.git ./build/svelte
-cd ./build/svelte
-yarn install
-RUN echo "VITE_API_URL=" > ./.env
-yarn build
-cd ../../
+# Prompt the user for confirmation
+read -p "Do you want to run the Panel Build Script? (yes/no): " answer
+
+# Proceed with the script only if the answer is "yes"
+if [ "$answer" = "yes" ]; then
+    rm -fr ./build/svelte
+    # PANEL
+    git clone https://github.com/02Gqbriel/videocms-svelte.git ./build/svelte
+    cd ./build/svelte
+    yarn install
+    echo "VITE_API_URL=" > ./.env
+    yarn build
+    cd ../../
+fi
 
 # DOCKER
 export DOCKER_BUILDKIT=1
