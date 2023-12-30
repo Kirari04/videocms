3ef90094b147d4813eb010cae60d80322e82cb58
kirari04 <lev@saminskij.ch>
2023-12-31 00:20:36 +0100

    new build settings

--- README.md
+++ README.md
@@ -15,23 +15,7 @@ View the latest documentation on <a href="https://documenter.getpostman.com/view
 ### Alpha
 
 The Alpha image contains the latest code with the full funtionallity.<br>
-Deploy with docker: `docker run -p 3000:3000 kirari04/videocms:alpha-1` <br>
-
-### Panel
-
-The Panel image contains the latest code with the full funtionallity and the frontend Panel.<br>
-Deploy with docker: `docker run -p 3000:3000 kirari04/videocms:panel` <br>
-
-### Demo
-
-The Demo image contains the latest code with limited functionallity. (encode & upload are disabled by default)<br>
-Deploy with docker: `docker run -p 3000:3000 kirari04/videocms:demo` <br>
-
-### Demo Panel
-
-The Demo Panel image contains the latest code with limited functionallity and the frontend Panel. (encode & upload are disabled by default)<br>
-Deploy with docker: `docker run -p 3000:3000 kirari04/videocms:demo_panel` <br>
-You can also use the live preview here: <a href="https://videocms.senpai.one/" target="_blank">videocms.senpai.one</a>
+Deploy with docker: `docker run -p 3000:3000 kirari04/videocms:alpha` <br>
 
 ## Images For Arm64 CPU's Like Raspberry Pi
 
@@ -39,14 +23,12 @@ just use the same images like before just add `_arm64` to the end of the tag. <b
 Here some examples
 
 ```
-docker run -p 3000:3000 kirari04/videocms:alpha-1_arm64
-docker run -p 3000:3000 kirari04/videocms:panel_arm64
-docker run -p 3000:3000 kirari04/videocms:demo_arm64
+docker run -p 3000:3000 kirari04/videocms:alpha_arm64
 ```
 
 ## Authentication / Default Admin
 
-Default username & password are `admin` and `12345678`
+Create a new user by running the command `./main.bin create:user` inside the container.
 
 ## Configuration
 
--- build/build.sh
+++ build/build.sh
@@ -5,19 +5,15 @@ bash scripts/\$default.sh
 read -p "Do you want to run the Binary Build Script? (yes/no): " answerbin
 if [ "$answerbin" = "yes" ]; then
     rm -rf ./build/cmd
-    # BINARIES
     mkdir -p ./build/cmd
-    # MAIN
     echo RUNNING GO BUILD MAIN
+    
     ## amd64
-    # echo RUNNING GO BUILD windows amd64
-    # CGO_ENABLED=0 GOOS=windows GOARCH=amd64 go build -ldflags "-linkmode external -extldflags -static" -a -installsuffix cgo -o build/cmd/main_windows_amd64.exe main.go
-    # sha256sum build/cmd/main_windows_amd64.exe > build/cmd/main_windows_amd64.exe.sha256sum
-    # gpg --detach-sig --armor build/cmd/main_windows_amd64.exe
     echo RUNNING GO BUILD linux amd64
     CGO_ENABLED=1 GOOS=linux GOARCH=amd64 go build -ldflags "-linkmode external -extldflags -static" -a -installsuffix cgo -o build/cmd/main_linux_amd64.bin main.go
     sha256sum build/cmd/main_linux_amd64.bin > build/cmd/main_linux_amd64.bin.sha256sum
     # gpg --detach-sig --armor build/cmd/main_linux_amd64.bin
+
     ## arm64
     echo RUNNING GO BUILD linux arm64
     CGO_ENABLED=1 CC=aarch64-linux-gnu-gcc-11 CC_FOR_TARGET=gcc-11-aarch64-linux-gnu GOOS=linux GOARCH=arm64 go build -ldflags "-linkmode external -extldflags -static" -a -installsuffix cgo -o build/cmd/main_linux_arm64.bin main.go
@@ -28,13 +24,8 @@ fi
 # DOCKER
 export DOCKER_BUILDKIT=1
 # echo RUNNING DOCKER BUILD AMD64
-# docker build . --platform linux/amd64 -f Dockerfile -t kirari04/videocms:alpha-1 --push
+docker build . --platform linux/amd64 -f Dockerfile -t kirari04/videocms:alpha --push
 # echo RUNNING DOCKER BUILD ARM64
-# docker build . --platform linux/arm64 -f Dockerfile.arm64 -t kirari04/videocms:alpha-1_arm64 --push
-
-echo RUNNING DOCKER BUILD AMD64 PANEL
-docker build . --platform linux/amd64 -f Dockerfile.panel -t kirari04/videocms:panel --push
-# echo RUNNING DOCKER BUILD ARM64 PANEL
-# docker build . --platform linux/arm64 -f Dockerfile.panel.arm64 -t kirari04/videocms:panel_arm64 --push
+docker build . --platform linux/arm64 -f Dockerfile.arm64 -t kirari04/videocms:alpha_arm64 --push
 
 echo "DONE"
