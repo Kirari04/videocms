05afc63588d626999ac11334c678c72f2b2fbed4
kirari04 <lev@saminskij.ch>
2024-01-03 22:25:20 +0100

    better server hostname validation

--- models/Server.go
+++ models/Server.go
@@ -5,15 +5,15 @@ import "time"
 type Server struct {
 	Model
 	UUID     string
-	Hostname string
+	Hostname string `gorm:"size:128;"`
 	Token    string `gorm:"size:512;" json:"-"`
 	LastPing time.Time
 }
 
 type ServerDeleteValidation struct {
-	ServerID uint `validate:"required,number"`
+	ServerID uint `validate:"required,number,gte=1"`
 }
 
 type ServerCreateValidation struct {
-	Hostname string `validate:"required,hostname"`
+	Hostname string `validate:"required,hostname,max=128"`
 }
