0380604e2d36287db82213b5387c5801dba502b1
kirari04 <lev@saminskij.ch>
2024-01-18 20:13:45 +0100

    implemented DeleteFileController

--- controllers/DeleteFileController.go
+++ controllers/DeleteFileController.go
@@ -4,19 +4,15 @@ import (
 	"ch/kirari04/videocms/helpers"
 	"ch/kirari04/videocms/logic"
 	"ch/kirari04/videocms/models"
-	"fmt"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/labstack/echo/v4"
 )
 
-func DeleteFileController(c *fiber.Ctx) error {
+func DeleteFileController(c echo.Context) error {
 	// parse & validate request
 	var fileValidation models.LinkDeleteValidation
-	if err := c.BodyParser(&fileValidation); err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString("Invalid body request format")
-	}
-	if errors := helpers.ValidateStruct(fileValidation); len(errors) > 0 {
-		return c.Status(fiber.StatusBadRequest).SendString(fmt.Sprintf("%s [%s] : %s", errors[0].FailedField, errors[0].Tag, errors[0].Value))
+	if status, err := helpers.Validate(c, &fileValidation); err != nil {
+		return c.String(status, err.Error())
 	}
 
 	// Business logic
@@ -24,10 +20,10 @@ func DeleteFileController(c *fiber.Ctx) error {
 		LinkIDs: []models.LinkDeleteValidation{
 			fileValidation,
 		},
-	}, c.Locals("UserID").(uint))
+	}, c.Get("UserID").(uint))
 
 	if err != nil {
-		return c.Status(status).SendString(err.Error())
+		return c.String(status, err.Error())
 	}
-	return c.SendStatus(status)
+	return c.NoContent(status)
 }
