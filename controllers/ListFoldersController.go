a4d21c0bf933c78cdbeb0e00673058e4045e3210
kirari04 <lev@saminskij.ch>
2024-01-18 21:29:55 +0100

    implemented ListFolders

--- controllers/ListFoldersController.go
+++ controllers/ListFoldersController.go
@@ -4,34 +4,24 @@ import (
 	"ch/kirari04/videocms/helpers"
 	"ch/kirari04/videocms/inits"
 	"ch/kirari04/videocms/models"
-	"fmt"
 	"log"
+	"net/http"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/labstack/echo/v4"
 )
 
-func ListFolders(c *fiber.Ctx) error {
+func ListFolders(c echo.Context) error {
 	// parse & validate request
 	var folderValidation models.FolderListValidation
-	if err := c.QueryParser(&folderValidation); err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString("Invalid body request format")
-	}
-
-	if errors := helpers.ValidateStruct(folderValidation); len(errors) > 0 {
-		return c.Status(fiber.StatusBadRequest).SendString(fmt.Sprintf("%s [%s] : %s", errors[0].FailedField, errors[0].Tag, errors[0].Value))
+	if status, err := helpers.Validate(c, &folderValidation); err != nil {
+		return c.String(status, err.Error())
 	}
 
 	//check if requested folder exists
 	if folderValidation.ParentFolderID > 0 {
 		res := inits.DB.First(&models.Folder{}, folderValidation.ParentFolderID)
 		if res.Error != nil {
-			return c.Status(400).JSON([]helpers.ValidationError{
-				{
-					FailedField: "ParentFolderID",
-					Tag:         "exists",
-					Value:       "Parent folder doesn't exist",
-				},
-			})
+			return c.String(http.StatusBadRequest, "Parent folder doesn't exist")
 		}
 	}
 
@@ -42,15 +32,15 @@ func ListFolders(c *fiber.Ctx) error {
 		Preload("User").
 		Where(&models.Folder{
 			ParentFolderID: folderValidation.ParentFolderID,
-			UserID:         c.Locals("UserID").(uint),
+			UserID:         c.Get("UserID").(uint),
 		}, "ParentFolderID", "UserID").
 		Order("name ASC").
 		Find(&folders)
 	if res.Error != nil {
 		log.Printf("Failed to query folder list: %v", res.Error)
-		return c.SendStatus(fiber.StatusInternalServerError)
+		return c.NoContent(http.StatusInternalServerError)
 	}
 
 	// return value
-	return c.JSON(folders)
+	return c.JSON(http.StatusOK, &folders)
 }
