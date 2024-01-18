db5c6e16492f1a2b5493a7c65aa4294394c038c6
kirari04 <lev@saminskij.ch>
2024-01-18 19:34:03 +0100

    migrated captcha helper

--- helpers/Captcha.go
+++ helpers/Captcha.go
@@ -5,11 +5,11 @@ import (
 	"errors"
 
 	recaptcha "github.com/dpapathanasiou/go-recaptcha"
-	"github.com/gofiber/fiber/v2"
 	hcaptcha "github.com/kirari04/go-hcaptcha"
+	"github.com/labstack/echo/v4"
 )
 
-func CaptchaValid(c *fiber.Ctx) (bool, error) {
+func CaptchaValid(c echo.Context) (bool, error) {
 	if !*config.ENV.CaptchaEnabled {
 		return true, nil
 	}
@@ -24,28 +24,28 @@ func CaptchaValid(c *fiber.Ctx) (bool, error) {
 	return false, errors.New("invalid CaptchaType set")
 }
 
-func recaptchaValidate(c *fiber.Ctx) (bool, error) {
+func recaptchaValidate(c echo.Context) (bool, error) {
 	// parse & validate request
 	type Validation struct {
 		Token string `validate:"required,min=1,max=1500" json:"g-recaptcha-response" form:"g-recaptcha-response"`
 	}
 	var validation Validation
-	if _, err := Validate(c, &validation, "body"); err != nil {
+	if _, err := Validate(c, &validation); err != nil {
 		return false, err
 	}
 
-	return recaptcha.Confirm(c.IP(), validation.Token)
+	return recaptcha.Confirm(c.RealIP(), validation.Token)
 }
 
-func hcaptchaValidate(c *fiber.Ctx) (bool, error) {
+func hcaptchaValidate(c echo.Context) (bool, error) {
 	// parse & validate request
 	type Validation struct {
 		Token string `validate:"required,min=1,max=1500" json:"h-captcha-response" form:"h-captcha-response"`
 	}
 	var validation Validation
-	if _, err := Validate(c, &validation, "body"); err != nil {
+	if _, err := Validate(c, &validation); err != nil {
 		return false, err
 	}
 
-	return hcaptcha.Confirm(c.IP(), validation.Token)
+	return hcaptcha.Confirm(c.RealIP(), validation.Token)
 }
