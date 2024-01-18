89179bea7e523942ae8b97e9d62740facf916c0e
kirari04 <lev@saminskij.ch>
2024-01-18 21:26:44 +0100

    implemented GetThumbnailData

--- controllers/GetThumbnailDataController.go
+++ controllers/GetThumbnailDataController.go
@@ -3,32 +3,28 @@ package controllers
 import (
 	"ch/kirari04/videocms/helpers"
 	"ch/kirari04/videocms/logic"
-	"fmt"
+	"net/http"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/labstack/echo/v4"
 )
 
-func GetThumbnailData(c *fiber.Ctx) error {
+func GetThumbnailData(c echo.Context) error {
 	type Request struct {
 		UUID string `validate:"required,uuid_rfc4122"`
 		FILE string `validate:"required"`
 	}
 	var requestValidation Request
-	if err := c.ParamsParser(&requestValidation); err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString("Invalid body request format")
+	if status, err := helpers.Validate(c, &requestValidation); err != nil {
+		return c.String(status, err.Error())
 	}
 
-	if errors := helpers.ValidateStruct(requestValidation); len(errors) > 0 {
-		return c.Status(fiber.StatusBadRequest).SendString(fmt.Sprintf("%s [%s] : %s", errors[0].FailedField, errors[0].Tag, errors[0].Value))
-	}
-
-	status, filePath, err := logic.GetThumbnailData(requestValidation.FILE, requestValidation.UUID)
+	_, filePath, err := logic.GetThumbnailData(requestValidation.FILE, requestValidation.UUID)
 	if err != nil {
-		return c.Type(".txt").SendStatus(fiber.StatusNotFound)
+		return c.NoContent(http.StatusNotFound)
 	}
 
-	if err := c.Status(status).SendFile(*filePath); err != nil {
-		return c.SendStatus(fiber.StatusNotFound)
+	if err := c.File(*filePath); err != nil {
+		return c.NoContent(http.StatusNotFound)
 	}
 	return nil
 }
