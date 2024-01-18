bd68d2aa24b79e2bf587aa0004eca6725f441d5f
kirari04 <lev@saminskij.ch>
2024-01-18 19:31:07 +0100

    migrated GetFileExample

--- controllers/GetFileExampleController.go
+++ controllers/GetFileExampleController.go
@@ -3,13 +3,13 @@ package controllers
 import (
 	"ch/kirari04/videocms/logic"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/labstack/echo/v4"
 )
 
-func GetFileExample(c *fiber.Ctx) error {
+func GetFileExample(c echo.Context) error {
 	status, response, err := logic.GetFileExample()
 	if err != nil {
-		return c.Status(status).SendString(err.Error())
+		return c.String(status, err.Error())
 	}
-	return c.Status(status).SendString(response)
+	return c.String(status, response)
 }
