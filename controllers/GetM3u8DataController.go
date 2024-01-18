d558f841893fc2664436cacb821ec3c0a3d64efa
kirari04 <lev@saminskij.ch>
2024-01-18 21:23:32 +0100

    implemented GetM3u8Data

--- controllers/GetM3u8DataController.go
+++ controllers/GetM3u8DataController.go
@@ -4,21 +4,22 @@ import (
 	"ch/kirari04/videocms/helpers"
 	"ch/kirari04/videocms/logic"
 	"fmt"
+	"net/http"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/labstack/echo/v4"
 )
 
-func GetM3u8Data(c *fiber.Ctx) error {
+func GetM3u8Data(c echo.Context) error {
 	var requestValidation logic.GetM3u8DataRequest
 	var requestValidationMuted logic.GetM3u8DataRequestMuted
-	requestValidation.UUID = c.Params("UUID")
-	requestValidation.AUDIOUUID = c.Params("AUDIOUUID")
-	requestValidation.JWT = c.Query("jwt", "")
+	requestValidation.UUID = c.Param("UUID")
+	requestValidation.AUDIOUUID = c.Param("AUDIOUUID")
+	requestValidation.JWT = c.QueryParam("jwt")
 	var JWT string
 	if requestValidation.AUDIOUUID != "" {
 		// validate audio stream
 		if errors := helpers.ValidateStruct(requestValidation); len(errors) > 0 {
-			return c.Status(fiber.StatusBadRequest).SendString(fmt.Sprintf("%s [%s] : %s", errors[0].FailedField, errors[0].Tag, errors[0].Value))
+			return c.String(http.StatusBadRequest, fmt.Sprintf("%s [%s] : %s", errors[0].FailedField, errors[0].Tag, errors[0].Value))
 		}
 		JWT = requestValidation.JWT
 	} else {
@@ -26,15 +27,15 @@ func GetM3u8Data(c *fiber.Ctx) error {
 		requestValidationMuted.UUID = requestValidation.UUID
 		requestValidationMuted.JWT = requestValidation.JWT
 		if errors := helpers.ValidateStruct(requestValidationMuted); len(errors) > 0 {
-			return c.Status(fiber.StatusBadRequest).SendString(fmt.Sprintf("%s [%s] : %s", errors[0].FailedField, errors[0].Tag, errors[0].Value))
+			return c.String(http.StatusBadRequest, fmt.Sprintf("%s [%s] : %s", errors[0].FailedField, errors[0].Tag, errors[0].Value))
 		}
 		JWT = requestValidationMuted.JWT
 	}
 
 	status, m3u8Str, err := logic.GetM3u8Data(requestValidation.UUID, requestValidation.AUDIOUUID, JWT)
 	if err != nil {
-		return c.Status(status).SendString(err.Error())
+		return c.String(status, err.Error())
 	}
 
-	return c.Status(status).SendString(*m3u8Str)
+	return c.String(status, *m3u8Str)
 }
