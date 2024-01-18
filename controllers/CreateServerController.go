b9a9ecaa4dbf3b0e069c824550ab25b11ccc8e87
kirari04 <lev@saminskij.ch>
2024-01-18 20:00:16 +0100

    migrated CreateServer

--- controllers/CreateServerController.go
+++ controllers/CreateServerController.go
@@ -4,33 +4,30 @@ import (
 	"ch/kirari04/videocms/helpers"
 	"ch/kirari04/videocms/inits"
 	"ch/kirari04/videocms/models"
-	"fmt"
-	"log"
+	"net/http"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
 	"github.com/thanhpk/randstr"
 )
 
-func CreateServer(c *fiber.Ctx) error {
+func CreateServer(c echo.Context) error {
 	// parse & validate request
 	var validatus models.ServerCreateValidation
-	if err := c.BodyParser(&validatus); err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString("Invalid body request format")
+	if status, err := helpers.Validate(c, &validatus); err != nil {
+		return c.String(status, err.Error())
 	}
 
-	if errors := helpers.ValidateStruct(validatus); len(errors) > 0 {
-		return c.Status(fiber.StatusBadRequest).SendString(fmt.Sprintf("%s [%s] : %s", errors[0].FailedField, errors[0].Tag, errors[0].Value))
-	}
 	var existing int64
 	if res := inits.DB.Model(&models.Server{}).Where(&models.Server{
 		Hostname: validatus.Hostname,
 	}).Count(&existing); res.Error != nil {
-		log.Println("Failed to count server", res.Error)
-		return c.SendStatus(fiber.StatusInternalServerError)
+		c.Logger().Error("Failed to count server", res.Error)
+		return c.NoContent(fiber.StatusInternalServerError)
 	}
 	if existing > 0 {
-		return c.Status(fiber.StatusBadRequest).SendString("Hostname already used")
+		return c.String(http.StatusBadRequest, "Hostname already used")
 	}
 
 	server := models.Server{
@@ -39,11 +36,11 @@ func CreateServer(c *fiber.Ctx) error {
 		Token:    randstr.Base62(512),
 	}
 	if res := inits.DB.Create(&server); res.Error != nil {
-		log.Println("Failed to create server", res.Error)
-		return c.SendStatus(fiber.StatusInternalServerError)
+		c.Logger().Error("Failed to create server", res.Error)
+		return c.NoContent(http.StatusInternalServerError)
 	}
 
-	return c.Status(fiber.StatusOK).JSON(fiber.Map{
+	return c.JSON(http.StatusOK, echo.Map{
 		"ID":       server.ID,
 		"UUID":     server.UUID,
 		"Hostname": server.Hostname,
