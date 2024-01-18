88beda3c33c755530c07e01b09c0452f44e45507
kirari04 <lev@saminskij.ch>
2024-01-18 21:37:32 +0100

    UpdateUserSettingsController

--- controllers/UpdateUserSettingsController.go
+++ controllers/UpdateUserSettingsController.go
@@ -5,44 +5,36 @@ import (
 	"ch/kirari04/videocms/inits"
 	"ch/kirari04/videocms/models"
 	"log"
+	"net/http"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/labstack/echo/v4"
 )
 
-func UpdateUserSettingsController(c *fiber.Ctx) error {
+func UpdateUserSettingsController(c echo.Context) error {
 	// parse & validate request
 	var validater models.UserSettingsUpdateValidation
-	if err := c.BodyParser(&validater); err != nil {
-		return c.Status(400).JSON([]helpers.ValidationError{
-			{
-				FailedField: "none",
-				Tag:         "none",
-				Value:       "Invalid body request format",
-			},
-		})
-	}
-	if errors := helpers.ValidateStruct(validater); len(errors) > 0 {
-		return c.Status(400).JSON(errors)
+	if status, err := helpers.Validate(c, &validater); err != nil {
+		return c.String(status, err.Error())
 	}
 
-	userId, ok := c.Locals("UserID").(uint)
+	userId, ok := c.Get("UserID").(uint)
 	if !ok {
 		log.Println("Failed to catch userID")
-		return c.SendStatus(fiber.StatusInternalServerError)
+		return c.NoContent(http.StatusInternalServerError)
 	}
 
 	var user models.User
 	if res := inits.DB.First(&user, userId); res.Error != nil {
 		log.Println("Failed to catch userID on db")
-		return c.SendStatus(fiber.StatusInternalServerError)
+		return c.NoContent(http.StatusInternalServerError)
 	}
 
 	user.Settings.EnablePlayerCaptcha = *validater.EnablePlayerCaptcha
 
 	if res := inits.DB.Save(&user); res.Error != nil {
 		log.Println("Failed to update user settings", res.Error)
-		return c.SendStatus(fiber.StatusInternalServerError)
+		return c.NoContent(http.StatusInternalServerError)
 	}
 
-	return c.SendStatus(fiber.StatusOK)
+	return c.NoContent(http.StatusOK)
 }
