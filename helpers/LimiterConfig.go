0c0d9320c24c38471c90c076e41fdc6d74de898b
kirari04 <lev@saminskij.ch>
2024-01-18 19:34:22 +0100

    migrated ratelimit config helper

--- helpers/LimiterConfig.go
+++ helpers/LimiterConfig.go
@@ -1,22 +1,31 @@
 package helpers
 
 import (
+	"net/http"
 	"time"
 
-	"github.com/gofiber/fiber/v2"
-	"github.com/gofiber/fiber/v2/middleware/limiter"
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+	"golang.org/x/time/rate"
 )
 
-func LimiterConfig(max int, expiration time.Duration) *limiter.Config {
-	return &limiter.Config{
-		Next:       LimiterWhitelistNext,
-		Max:        max,
-		Expiration: expiration,
-		KeyGenerator: func(c *fiber.Ctx) string {
-			return c.IP()
+func LimiterConfig(rate rate.Limit, burst int, expiration time.Duration) *middleware.RateLimiterConfig {
+	return &middleware.RateLimiterConfig{
+		Skipper: LimiterWhitelistNext,
+		Store: middleware.NewRateLimiterMemoryStoreWithConfig(
+			middleware.RateLimiterMemoryStoreConfig{Rate: rate, Burst: burst, ExpiresIn: expiration},
+		),
+		IdentifierExtractor: func(c echo.Context) (string, error) {
+			id := c.RealIP()
+			return id, nil
 		},
-		LimitReached: func(c *fiber.Ctx) error {
-			return c.Status(fiber.StatusTooManyRequests).SendString("Too fast")
+		ErrorHandler: func(c echo.Context, err error) error {
+			c.Logger().Error("Ratelimit Error", err)
+			return c.NoContent(http.StatusInternalServerError)
+		},
+		DenyHandler: func(c echo.Context, identifier string, err error) error {
+			return c.String(http.StatusTooManyRequests, "Too fast")
 		},
 	}
+
 }
