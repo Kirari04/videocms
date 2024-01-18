d44e287f7c903fe4345a17cc3f2289cd21483485
kirari04 <lev@saminskij.ch>
2024-01-18 21:27:31 +0100

    implemented GetUserSettingsController

--- controllers/GetUserSettingsController.go
+++ controllers/GetUserSettingsController.go
@@ -4,24 +4,25 @@ import (
 	"ch/kirari04/videocms/inits"
 	"ch/kirari04/videocms/models"
 	"log"
+	"net/http"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/labstack/echo/v4"
 )
 
-func GetUserSettingsController(c *fiber.Ctx) error {
-	userId, ok := c.Locals("UserID").(uint)
+func GetUserSettingsController(c echo.Context) error {
+	userId, ok := c.Get("UserID").(uint)
 	if !ok {
 		log.Println("Failed to catch userID")
-		return c.SendStatus(fiber.StatusInternalServerError)
+		return c.NoContent(http.StatusInternalServerError)
 	}
 
 	var user models.User
 	if res := inits.DB.First(&user, userId); res.Error != nil {
 		log.Println("Failed to catch userID on db")
-		return c.SendStatus(fiber.StatusInternalServerError)
+		return c.NoContent(http.StatusInternalServerError)
 	}
 
-	return c.JSON(fiber.Map{
+	return c.JSON(http.StatusOK, echo.Map{
 		"EnablePlayerCaptcha": user.Settings.EnablePlayerCaptcha,
 	})
 }
