5ec9c2d9e0dcd247246c7eccc536f7ba8239234b
kirari04 <lev@saminskij.ch>
2024-01-18 19:54:13 +0100

    migrated IsAdmin middleware

--- middlewares/IsAdmin.go
+++ middlewares/IsAdmin.go
@@ -1,16 +1,24 @@
 package middlewares
 
 import (
+	"net/http"
+
 	"github.com/gofiber/fiber/v2"
+	"github.com/labstack/echo/v4"
 )
 
-func IsAdmin(c *fiber.Ctx) error {
-	isAdmin, ok := c.Locals("Admin").(bool)
-	if !ok {
-		return c.SendStatus(fiber.StatusInternalServerError)
-	}
-	if !isAdmin {
-		return c.Status(fiber.StatusForbidden).SendString("Not Permitted")
-	}
-	return c.Next()
+func IsAdmin() echo.MiddlewareFunc {
+	return echo.MiddlewareFunc(func(next echo.HandlerFunc) echo.HandlerFunc {
+		return func(c echo.Context) error {
+			isAdmin, ok := c.Get("Admin").(bool)
+			if !ok {
+				c.Logger().Error("Failed to catch Admin")
+				return c.NoContent(fiber.StatusInternalServerError)
+			}
+			if !isAdmin {
+				return c.String(http.StatusForbidden, "Not Permitted")
+			}
+			return next(c)
+		}
+	})
 }
