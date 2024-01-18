7704f7bac1305a06446e9cb7ee630d8afd411143
kirari04 <lev@saminskij.ch>
2024-01-18 20:15:52 +0100

    implemented DeleteFolders

--- controllers/DeleteFoldersController.go
+++ controllers/DeleteFoldersController.go
@@ -4,26 +4,21 @@ import (
 	"ch/kirari04/videocms/helpers"
 	"ch/kirari04/videocms/logic"
 	"ch/kirari04/videocms/models"
-	"fmt"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/labstack/echo/v4"
 )
 
-func DeleteFolders(c *fiber.Ctx) error {
+func DeleteFolders(c echo.Context) error {
 	// parse & validate request
 	var folderValidation models.FoldersDeleteValidation
-	if err := c.BodyParser(&folderValidation); err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString("Invalid body request format")
-
-	}
-	if errors := helpers.ValidateStruct(folderValidation); len(errors) > 0 {
-		return c.Status(fiber.StatusBadRequest).SendString(fmt.Sprintf("%s [%s] : %s", errors[0].FailedField, errors[0].Tag, errors[0].Value))
+	if status, err := helpers.Validate(c, &folderValidation); err != nil {
+		return c.String(status, err.Error())
 	}
 
 	// Business logic
-	status, err := logic.DeleteFolders(&folderValidation, c.Locals("UserID").(uint))
+	status, err := logic.DeleteFolders(&folderValidation, c.Get("UserID").(uint))
 	if err != nil {
-		return c.Status(status).SendString(err.Error())
+		return c.String(status, err.Error())
 	}
-	return c.SendStatus(status)
+	return c.NoContent(status)
 }
