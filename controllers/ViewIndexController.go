1448a97eab1075b5f074895d3f11c6c5b0c6e03e
kirari04 <lev@saminskij.ch>
2024-01-18 21:39:07 +0100

    ViewIndex

--- controllers/ViewIndexController.go
+++ controllers/ViewIndexController.go
@@ -5,21 +5,23 @@ import (
 	"ch/kirari04/videocms/inits"
 	"ch/kirari04/videocms/models"
 	"fmt"
+	"net/http"
 
 	"github.com/gofiber/fiber/v2"
+	"github.com/labstack/echo/v4"
 )
 
-func ViewIndex(c *fiber.Ctx) error {
+func ViewIndex(c echo.Context) error {
 	var link models.Link
 	if res := inits.DB.First(&link); res.Error != nil {
-		return c.Render("index", fiber.Map{
+		return c.Render(http.StatusOK, "index", fiber.Map{
 			"ExampleVideo":         fmt.Sprintf("/%v", "notfound"),
 			"AppName":              config.ENV.AppName,
 			"ProjectDocumentation": config.ENV.ProjectDocumentation,
 			"ProjectDownload":      config.ENV.ProjectDownload,
 		})
 	}
-	return c.Render("index", fiber.Map{
+	return c.Render(http.StatusOK, "index", fiber.Map{
 		"ExampleVideo":         fmt.Sprintf("/%v", link.UUID),
 		"AppName":              config.ENV.AppName,
 		"ProjectDocumentation": config.ENV.ProjectDocumentation,
