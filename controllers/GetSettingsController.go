8457e49d64506df42e3eb8f0f0e4358db3fb544a
kirari04 <lev@saminskij.ch>
2024-01-18 21:24:19 +0100

    implemented GetSettings

--- controllers/GetSettingsController.go
+++ controllers/GetSettingsController.go
@@ -4,21 +4,22 @@ import (
 	"ch/kirari04/videocms/inits"
 	"ch/kirari04/videocms/models"
 	"log"
+	"net/http"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/labstack/echo/v4"
 )
 
-func GetSettings(c *fiber.Ctx) error {
-	_, ok := c.Locals("UserID").(uint)
+func GetSettings(c echo.Context) error {
+	_, ok := c.Get("UserID").(uint)
 	if !ok {
 		log.Println("Failed to catch user")
-		return c.SendStatus(fiber.StatusInternalServerError)
+		return c.NoContent(http.StatusInternalServerError)
 	}
 
 	var setting models.Setting
 	if res := inits.DB.FirstOrCreate(&setting); res.Error != nil {
 		log.Fatalln("Failed to get settings", res.Error)
-		return c.SendStatus(fiber.StatusInternalServerError)
+		return c.NoContent(http.StatusInternalServerError)
 	}
-	return c.Status(fiber.StatusOK).JSON(&setting)
+	return c.JSON(http.StatusOK, &setting)
 }
