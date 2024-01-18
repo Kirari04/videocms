39a0a8a56372d4dfe465fa1bae51660a67813767
kirari04 <lev@saminskij.ch>
2024-01-18 19:29:07 +0100

    removed fiber

--- controllers/AuthRefreshController.go
+++ controllers/AuthRefreshController.go
@@ -5,20 +5,19 @@ import (
 	"net/http"
 	"strings"
 
-	"github.com/gofiber/fiber/v2"
 	"github.com/labstack/echo/v4"
 )
 
 func AuthRefresh(c echo.Context) error {
 	bearer := c.Request().Header.Get("Authorization")
 	if bearer == "" {
-		return c.NoContent(fiber.StatusForbidden)
+		return c.NoContent(http.StatusForbidden)
 	}
 	bearerHeader := strings.Split(bearer, " ")
 	tokenString := bearerHeader[len(bearerHeader)-1]
 	newTokenString, expirationTime, err := auth.RefreshJWT(tokenString)
 	if err != nil {
-		return c.String(fiber.StatusForbidden, err.Error())
+		return c.String(http.StatusForbidden, err.Error())
 	}
 
 	return c.JSON(http.StatusOK, echo.Map{
