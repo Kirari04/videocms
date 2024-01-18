ffb24ec40513dab7083a6e32eb8485674db854bb
kirari04 <lev@saminskij.ch>
2024-01-18 21:37:49 +0100

    UpdateWebhook

--- controllers/UpdateWebhookController.go
+++ controllers/UpdateWebhookController.go
@@ -4,28 +4,23 @@ import (
 	"ch/kirari04/videocms/helpers"
 	"ch/kirari04/videocms/logic"
 	"ch/kirari04/videocms/models"
-	"fmt"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/labstack/echo/v4"
 )
 
-func UpdateWebhook(c *fiber.Ctx) error {
+func UpdateWebhook(c echo.Context) error {
 	// parse & validate request
 	var validation models.WebhookUpdateValidation
-	if err := c.BodyParser(&validation); err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString("Invalid body request format")
+	if status, err := helpers.Validate(c, &validation); err != nil {
+		return c.String(status, err.Error())
 	}
 
-	if errors := helpers.ValidateStruct(validation); len(errors) > 0 {
-		return c.Status(fiber.StatusBadRequest).SendString(fmt.Sprintf("%s [%s] : %s", errors[0].FailedField, errors[0].Tag, errors[0].Value))
-	}
-
-	userID := c.Locals("UserID").(uint)
+	userID := c.Get("UserID").(uint)
 
 	status, response, err := logic.UpdateWebhook(&validation, userID)
 	if err != nil {
-		return c.Status(status).SendString(err.Error())
+		return c.String(status, err.Error())
 	}
 
-	return c.Status(status).SendString(response)
+	return c.String(status, response)
 }
