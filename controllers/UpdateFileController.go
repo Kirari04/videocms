d48d19e804b7d1bba799dfe4b1069e88e96789dc
kirari04 <lev@saminskij.ch>
2024-01-18 21:35:09 +0100

    UpdateFile

--- controllers/UpdateFileController.go
+++ controllers/UpdateFileController.go
@@ -5,47 +5,27 @@ import (
 	"ch/kirari04/videocms/inits"
 	"ch/kirari04/videocms/models"
 	"log"
+	"net/http"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/labstack/echo/v4"
 )
 
-func UpdateFile(c *fiber.Ctx) error {
+func UpdateFile(c echo.Context) error {
 	// parse & validate request
 	var linkValidation models.LinkUpdateValidation
-	if err := c.BodyParser(&linkValidation); err != nil {
-		return c.Status(400).JSON([]helpers.ValidationError{
-			{
-				FailedField: "none",
-				Tag:         "none",
-				Value:       "Invalid body request format",
-			},
-		})
-	}
-	if errors := helpers.ValidateStruct(linkValidation); len(errors) > 0 {
-		return c.Status(400).JSON(errors)
+	if status, err := helpers.Validate(c, &linkValidation); err != nil {
+		return c.String(status, err.Error())
 	}
 
 	var dbLink models.Link
 	//check if requested file /link id exists
 	if res := inits.DB.First(&dbLink, linkValidation.LinkID); res.Error != nil {
-		return c.Status(400).JSON([]helpers.ValidationError{
-			{
-				FailedField: "LinkID",
-				Tag:         "exists",
-				Value:       "File doesn't exist",
-			},
-		})
+		return c.String(http.StatusBadRequest, "File doesn't exist")
 	}
 
 	if linkValidation.ParentFolderID > 0 {
 		if res := inits.DB.First(&models.Folder{}, linkValidation.ParentFolderID); res.Error != nil {
-			return c.Status(400).JSON([]helpers.ValidationError{
-				{
-					FailedField: "ParentFolderID",
-					Tag:         "exists",
-					Value:       "Parent folder doesn't exist",
-				},
-			})
+			return c.String(http.StatusBadRequest, "Parent folder doesn't exist")
 		}
 	}
 
@@ -54,8 +34,8 @@ func UpdateFile(c *fiber.Ctx) error {
 	dbLink.ParentFolderID = linkValidation.ParentFolderID
 	if res := inits.DB.Save(&dbLink); res.Error != nil {
 		log.Printf("Failed to update link: %v", res.Error)
-		return c.SendStatus(fiber.StatusInternalServerError)
+		return c.NoContent(http.StatusInternalServerError)
 	}
 
-	return c.SendStatus(fiber.StatusOK)
+	return c.NoContent(http.StatusOK)
 }
