db0fb235aeeece0881d705334b6395fb597afbcf
kirari04 <lev@saminskij.ch>
2024-01-18 21:38:09 +0100

    UpdateWebPage

--- controllers/UpdateWebPageController.go
+++ controllers/UpdateWebPageController.go
@@ -5,42 +5,39 @@ import (
 	"ch/kirari04/videocms/inits"
 	"ch/kirari04/videocms/models"
 	"errors"
-	"fmt"
 	"log"
+	"net/http"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/labstack/echo/v4"
 	"gorm.io/gorm"
 )
 
-func UpdateWebPage(c *fiber.Ctx) error {
+func UpdateWebPage(c echo.Context) error {
 	// parse & validate request
 	var validatus models.WebPageUpdateValidation
-	if err := c.BodyParser(&validatus); err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString("Invalid body request format")
+	if status, err := helpers.Validate(c, &validatus); err != nil {
+		return c.String(status, err.Error())
 	}
 
-	if errors := helpers.ValidateStruct(validatus); len(errors) > 0 {
-		return c.Status(fiber.StatusBadRequest).SendString(fmt.Sprintf("%s [%s] : %s", errors[0].FailedField, errors[0].Tag, errors[0].Value))
-	}
 	var existing int64
 	if res := inits.DB.Model(&models.WebPage{}).
 		Where("id != ?", validatus.WebPageID).
 		Where("path = ?", validatus.Path).
 		Count(&existing); res.Error != nil {
 		log.Println("Failed to count webpage path", res.Error)
-		return c.SendStatus(fiber.StatusInternalServerError)
+		return c.NoContent(http.StatusInternalServerError)
 	}
 	if existing > 0 {
-		return c.Status(fiber.StatusBadRequest).SendString("Path already used")
+		return c.String(http.StatusBadRequest, "Path already used")
 	}
 
 	var webPage models.WebPage
 	if res := inits.DB.First(&webPage, validatus.WebPageID); res.Error != nil {
 		if errors.Is(res.Error, gorm.ErrRecordNotFound) {
-			return c.Status(fiber.StatusNotFound).SendString("Webpage not found")
+			return c.String(http.StatusNotFound, "Webpage not found")
 		}
 		log.Println("Failed to find webpage", res.Error)
-		return c.SendStatus(fiber.StatusInternalServerError)
+		return c.NoContent(http.StatusInternalServerError)
 	}
 
 	webPage.Path = validatus.Path
@@ -50,8 +47,8 @@ func UpdateWebPage(c *fiber.Ctx) error {
 
 	if res := inits.DB.Save(&webPage); res.Error != nil {
 		log.Println("Failed to update webpage", res.Error)
-		return c.SendStatus(fiber.StatusInternalServerError)
+		return c.NoContent(http.StatusInternalServerError)
 	}
 
-	return c.Status(fiber.StatusOK).SendString("ok")
+	return c.String(http.StatusOK, "ok")
 }
