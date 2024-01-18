0e4ccc4a522faa9b4a4d2a86afe4690e009fe02a
kirari04 <lev@saminskij.ch>
2024-01-18 19:42:42 +0100

    migrated Auth middleware

--- middlewares/Auth.go
+++ middlewares/Auth.go
@@ -2,27 +2,30 @@ package middlewares
 
 import (
 	"ch/kirari04/videocms/auth"
+	"net/http"
 	"strings"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/labstack/echo/v4"
 )
 
-func Auth(c *fiber.Ctx) error {
-	bearer := c.GetReqHeaders()["Authorization"]
-	if len(bearer) == 0 || bearer[0] == "" {
-		return c.Status(fiber.StatusForbidden).SendString("No JWT Token")
+func Auth(next, stop echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		bearer := c.Request().Header.Get("Authorization")
+		if bearer == "" {
+			return c.String(http.StatusForbidden, "No JWT Token")
+		}
+		bearerHeader := strings.Split(bearer, " ")
+		tokenString := bearerHeader[len(bearerHeader)-1]
+		token, claims, err := auth.VerifyJWT(tokenString)
+		if err != nil {
+			return c.String(http.StatusForbidden, "Invalid JWT Token")
+		}
+		if !token.Valid {
+			return c.String(http.StatusForbidden, "Expired JWT Token")
+		}
+		c.Set("Username", claims.Username)
+		c.Set("UserID", claims.UserID)
+		c.Set("Admin", claims.Admin)
+		return next(c)
 	}
-	bearerHeader := strings.Split(bearer[0], " ")
-	tokenString := bearerHeader[len(bearerHeader)-1]
-	token, claims, err := auth.VerifyJWT(tokenString)
-	if err != nil {
-		return c.Status(fiber.StatusForbidden).SendString("Invalid JWT Token")
-	}
-	if !token.Valid {
-		return c.Status(fiber.StatusForbidden).SendString("Expired JWT Token")
-	}
-	c.Locals("Username", claims.Username)
-	c.Locals("UserID", claims.UserID)
-	c.Locals("Admin", claims.Admin)
-	return c.Next()
 }
