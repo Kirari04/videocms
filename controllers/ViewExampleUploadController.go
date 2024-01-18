3efd477104b95ad6a0122c0e028ab6a92707c934
kirari04 <lev@saminskij.ch>
2024-01-18 21:38:36 +0100

    ViewExampleUpload

--- controllers/ViewExampleUploadController.go
+++ controllers/ViewExampleUploadController.go
@@ -2,12 +2,13 @@ package controllers
 
 import (
 	"ch/kirari04/videocms/config"
+	"net/http"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/labstack/echo/v4"
 )
 
-func ViewExampleUpload(c *fiber.Ctx) error {
-	return c.Render("examples/upload", fiber.Map{
+func ViewExampleUpload(c echo.Context) error {
+	return c.Render(http.StatusOK, "examples/upload", echo.Map{
 		"AppName": config.ENV.AppName,
 	})
 }
