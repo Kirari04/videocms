fbe86562db6cf485452960db5fc9fd4afa36d973
kirari04 <lev@saminskij.ch>
2024-01-18 21:14:01 +0100

    implemented DeleteWebPage

--- controllers/DeleteWebPageController.go
+++ controllers/DeleteWebPageController.go
@@ -4,31 +4,27 @@ import (
 	"ch/kirari04/videocms/helpers"
 	"ch/kirari04/videocms/inits"
 	"ch/kirari04/videocms/models"
-	"fmt"
 	"log"
+	"net/http"
 
-	"github.com/gofiber/fiber/v2"
+	"github.com/labstack/echo/v4"
 )
 
-func DeleteWebPage(c *fiber.Ctx) error {
+func DeleteWebPage(c echo.Context) error {
 	// parse & validate request
 	var validatus models.WebPageDeleteValidation
-	if err := c.BodyParser(&validatus); err != nil {
-		return c.Status(fiber.StatusBadRequest).SendString("Invalid body request format")
-	}
-
-	if errors := helpers.ValidateStruct(validatus); len(errors) > 0 {
-		return c.Status(fiber.StatusBadRequest).SendString(fmt.Sprintf("%s [%s] : %s", errors[0].FailedField, errors[0].Tag, errors[0].Value))
+	if status, err := helpers.Validate(c, &validatus); err != nil {
+		return c.String(status, err.Error())
 	}
 
 	res := inits.DB.Delete(&models.WebPage{}, validatus.WebPageID)
 	if res.Error != nil {
 		log.Println("Failed to delete webpage", res.Error)
-		return c.SendStatus(fiber.StatusInternalServerError)
+		return c.NoContent(http.StatusInternalServerError)
 	}
 	if res.RowsAffected <= 0 {
-		return c.Status(fiber.StatusBadRequest).SendString("Webpage not found")
+		return c.String(http.StatusBadRequest, "Webpage not found")
 	}
 
-	return c.Status(fiber.StatusOK).SendString("ok")
+	return c.String(http.StatusOK, "ok")
 }
