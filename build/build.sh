e558118f3757e7a07861f8ee722f40316ccf7778
kirari04 <lev@saminskij.ch>
2024-06-25 21:36:08 +0200

    update build script

--- build/build.sh
+++ build/build.sh
@@ -4,14 +4,15 @@
 export DOCKER_BUILDKIT=1
 
 # Prompt the user for confirmation
-read -p "Do you want to run the Binary Build Script? (yes/no): " answerbin
+read -p "Do you want to run the Build for linux/amd64? (yes/no): " answerbin
+read -p "Do you want to run the Build for linux/arm64? (yes/no): " answerbinarm
 if [ "$answerbin" = "yes" ]; then
     # echo RUNNING DOCKER BUILD AMD64
     docker build . --platform linux/amd64 -f Dockerfile -t kirari04/videocms:alpha --push
     # gpg --detach-sig --armor build/cmd/main_linux_amd64.bin
 fi
 
-if [ "$answerbin" = "yes" ]; then
+if [ "$answerbinarm" = "yes" ]; then
     ## arm64
     # echo RUNNING DOCKER BUILD ARM64
     docker build . --platform linux/arm64 -f Dockerfile.arm64 -t kirari04/videocms:alpha_arm64 --push
